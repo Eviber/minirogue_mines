18f4b71581e099092502c59807296b7541c53d42
Youva GAUDE <ygaude@student.42.fr>
2017-11-15 17:32:00 +0100

    add loot.py

---
+++ loot.py
@@ -0,0 +1,13 @@
+from random import randint
+from env import *
+
+class Loot(Entity):
+    def __init__(self, env, x = False, y = False):
+        if x and y:
+            self.x, self.y = x, y
+        else:
+            self.x, self.y = env.getRandPos()
+        self.char = '*'
+        self.value = randint(1, 50)
+    def getValue(self):
+        return self.value
